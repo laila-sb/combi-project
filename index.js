c074c2cecaaba089135567f2d4c5820ada759131
Laila Alboloushi <laila.sb@outlook.com>
2024-07-18 18:59:12 +0200

    email validation working + added css

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const InputFirstName = document.getElementById("first-name-input");
 const InputLastName = document.getElementById("last-name-input");
 const InputEmail = document.getElementById("email-input");
-const submitButton1 = document.getElementById("submit-button1");
+const submitButton1 = document.getElementById("submit-button");
 const addButton = document.getElementById("add-button");
-const listContainer = document.getElementById('list-container')
-
-
+const listContainer = document.getElementById("list-container");
 
 function submitPersonalInfo() {
+  // const firstNameError = document.getElementById('first-name-feedback')
+  // const firstLastError = document.getElementById('last-name-feedback')
 
-}
-
-function validEmail() {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const email = InputEmail.value
-    const span = document.createElement('span')
-
-  if (email.includes(emailRegex)) {
-    span === input
-    console.log("please enter a valid email");
+  if (!InputFirstName.value && !InputLastName.value && !InputEmail.value) {
+    alert("Please fill in all required information");
   }
 }
+
+
+function validEmail() {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const email = InputEmail.value;
+  const span = document.createElement("span");
+  const emailError = document.getElementById("email-feedback");
+
+  if (!email.match(emailRegex)) {
+    emailError.innerHTML = "Invalid email entered";
+    // alert("please enter a valid email");
+    return false;
+  }
+  return true;
+}
+
 //function to check if first + last names entered
 // function to check if email is valid
 
@@ -28,6 +36,6 @@ function validEmail() {
 
 //function to add subscription to subs inventory
 
-function addToList(){
-    if (submit)
+function addToList() {
+  if (submit);
 }
